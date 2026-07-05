81831fb1ccdbd36411daf3cef506fbc36a98a568
ProfileForge Builder <builder@profileforge.local>
2026-07-05 17:29:06 +0530

    fix: add missing flutter_riverpod import to mission_card.dart

--- lib/ui/widgets/mission_card.dart
+++ lib/ui/widgets/mission_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../theme/app_theme.dart';
 import '../../providers/app_providers.dart';
