4fd3244b5c035d9d04b636250e2895bf1c0436cb
boom650 <boom650@users.noreply.github.com>
2026-08-01 15:34:17 +0530

    chore: remove 9 unused files (dead code cleanup)
    - Removed unused network_providers, localizations, galaxy_chart
    - Removed unused AnimationService, streak_card, remote_body_double
    - Removed unused background_sync, sync_status_widget, geo_repository
    - Kept referenced files (premium_animations, onboarding_state_machine, sync_table)

--- lib/core/network/network_providers.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:profileforge/core/network/api_client.dart';
-
-final apiClientProvider = Provider<ApiClient>((ref) => ApiClient());
