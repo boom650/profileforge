ab883483e23071105a4d1726756d0be18858227d
boom650 <boom650@users.noreply.github.com>
2026-08-02 08:50:14 +0530

    feat: added shimmer skeleton to Quests screen

--- lib/features/quests/presentation/quests_screen.dart
+++ lib/features/quests/presentation/quests_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
+import 'package:shimmer/shimmer.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../../core/widgets/premium_widgets.dart';
 import '../../../core/widgets/tap_scale.dart';
@@ -234,7 +235,7 @@ class QuestsScreen extends ConsumerWidget {
             ],
           );
         },
-        loading: () => const Center(child: CircularProgressIndicator(color: Palette.primary)),
+        loading: () => const Center(child: ShimmerLoader.missions()),
         error: (e, _) => Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
